984f1483a0e4d9fdbc71d4c93b85df7add29e96c
Fernando Rodriguez <frodriguez.developer@outlook.com>
2015-02-19 19:07:08 -0500

    Switched bootloader to GRUB2.

--- tools/mkimage/mkimage.sh
+++ tools/mkimage/mkimage.sh
@@ -2,10 +2,9 @@
 
 cd $(dirname $0)
 cp ../../files/hydrax-fs.img ./hydrax-fs.img
-/sbin/debugfs -w hydrax-fs.img -R "rm hydrax"
-/sbin/debugfs -w hydrax-fs.img -R "rm initrd"
-/sbin/debugfs -w hydrax-fs.img -R "write ../../kernel/hydrax hydrax"
-/sbin/debugfs -w hydrax-fs.img -R "write ../../kernel/initrd.img initrd"
+/bin/echo -e 	"cd boot\n" \
+		"write ../../kernel/hydrax hydrax\n" \
+		"write ../../kernel/initrd.img initrd" | /sbin/debugfs -w hydrax-fs.img
 /sbin/e2fsck -f hydrax-fs.img 
 dd if=../../files/hydrax-pt.img of=../../hydrax.img
 dd if=hydrax-fs.img of=../../hydrax.img seek=2048

8c677603689f516c059f338b1f3d017c3a150894
Fernando Rodriguez <frodriguez.developer@outlook.com>
2015-02-25 16:23:17 -0500

    Modifications to GCC build script.
    Created patches for GCC and binutils and changed the build
    scripts to use them. Also added some color to the build
    script.

--- tools/scripts/make_gcc.sh
+++ tools/scripts/make_gcc.sh
@@ -3,7 +3,8 @@
 SCRIPTPATH=$( cd $(dirname $0)/.. ; pwd -P )
 cd $SCRIPTPATH
 
-
+BULLET="\033[0;32m *\033[0m"
+REDBUL="\033[0;31m !!\033[0m"
 PREFIX="$SCRIPTPATH"
 TARGET=i386-hydrax
 #PATH="$PREFIX/bin:$PATH"
@@ -11,7 +12,7 @@ MAKE_OPTS=-j2
 
 last_error=0
 
-echo -e "Copying system includes..."
+echo -e "$BULLET Copying system includes..."
 scripts/copy_headers.sh > /dev/null || last_error=1
 if [ "$last_error" == "1" ]; then
 	echo -e "Error copying headers!"
@@ -20,18 +21,18 @@ fi
 
 mkdir -p tmp
 cd tmp
-echo -e "Fetching binutils-2.24.tar.gz..."
+echo -e "$BULLET Fetching binutils-2.24.tar.gz..."
 wget -c http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz 2> /dev/null
-echo -e "Fetching gcc-4.8.4.tar.gz..."
+echo -e "$BULLET Fetching gcc-4.8.4.tar.gz..."
 wget -c ftp://ftp.gnu.org/gnu/gcc/gcc-4.8.4/gcc-4.8.4.tar.gz 2> /dev/null
-echo -e "Fetching gmp-6.0.0a.tar.lz..."
+echo -e "$BULLET Fetching gmp-6.0.0a.tar.lz..."
 wget -c https://gmplib.org/download/gmp/gmp-6.0.0a.tar.lz 2> /dev/null
-echo -e "Fetching mpfr-3.1.2.tar.xz..."
+echo -e "$BULLET Fetching mpfr-3.1.2.tar.xz..."
 wget -c http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.xz 2> /dev/null
-echo -e "Fetching mpc-1.0.3.tar.gz..."
+echo -e "$BULLET Fetching mpc-1.0.3.tar.gz..."
 wget -c ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz 2> /dev/null
 
-echo -e "Cleaning up working directory..."
+echo -e "$BULLET Cleaning up working directory..."
 cd ..
 mkdir -p src
 cd src
@@ -43,18 +44,28 @@ rm -fr mpc-1.0.3
 rm -fr build-binutils
 rm -fr build-gcc
 
-echo -e "Unpacking binutils-2.24.tar.gz..."
+echo -e "$BULLET Unpacking binutils-2.24.tar.gz..."
 tar -xf ../tmp/binutils-2.24.tar.gz
 
-echo -e "Copying config files..."
-cp binutils/config.sub binutils-2.24/config.sub
-cp binutils/config.bfd binutils-2.24/bfd/config.bfd
-cp binutils/gas_configure.tgt binutils-2.24/gas/configure.tgt
-cp binutils/ld_configure.tgt binutils-2.24/ld/configure.tgt
-cp binutils/elf_i386_hydrax.sh binutils-2.24/ld/emulparams/elf_i386_hydrax.sh
-cp binutils/Makefile.am binutils-2.24/ld/Makefile.am
+#echo -e "Copying config files..."
+#cp binutils/config.sub binutils-2.24/config.sub
+#cp binutils/config.bfd binutils-2.24/bfd/config.bfd
+#cp binutils/gas_configure.tgt binutils-2.24/gas/configure.tgt
+#cp binutils/ld_configure.tgt binutils-2.24/ld/configure.tgt
+#cp binutils/elf_i386_hydrax.sh binutils-2.24/ld/emulparams/elf_i386_hydrax.sh
+#cp binutils/Makefile.am binutils-2.24/ld/Makefile.am
+#diff -rupN ../binutils-tmp/binutils-2.24/ binutils-2.24/ > binutils-2.24.patch
 
-echo -e "Running automake..."
+echo -e "$BULLET Applying binutils-2.24.patch..."
+cd binutils-2.24
+patch -p1 < ../patches/binutils-2.24.patch > /dev/null || last_error=1
+if [ "$last_error" == "1" ]; then
+	echo -e "$REDBUL Error applying patches/binutils-2.24.patch!!"
+	exit -1
+fi
+cd ..
+
+echo -e "$BULLET Running automake..."
 cd binutils-2.24/ld
 aclocal-1.13
 automake-1.13
@@ -62,89 +73,101 @@ cd ../..
 
 if [ "1" == "1" ]; then
 echo -e "\n"
-echo -e "Configuring binutils..."
+echo -e "$BULLET Configuring binutils..."
 mkdir -p build/binutils
 cd build/binutils
 ../../binutils-2.24/configure --target=$TARGET --prefix="$PREFIX" \
 	--with-sysroot="$PREFIX" --disable-nls --disable-werror || last_error=1
 if [ "$last_error" == "1" ]; then
-	echo -e "Error configuring binutils!"
+	echo -e "$REDBUL Error configuring binutils!"
 	exit -1
 fi
 
-echo -e "Compiling binutils..."
+echo -e "$BULLET Compiling binutils..."
 make $MAKE_OPTS || last_error=1
 if [ "$last_error" == "1" ]; then
-	echo -e "Error compiling binutils!"
+	echo -e "$REDBUL Error compiling binutils!"
 	exit -1
 fi
+echo -e "$BULLET Installing binutils-2.24..."
 make install || last_error=1
 if [ "$last_error" == "1" ]; then
-	echo -e "Error installing binutils!"
+	echo -e "$REDBUL Error installing binutils!"
 	exit -1
 fi
 
-echo -e "Cleaning up binutils..."
+echo -e "$BULLET Cleaning up binutils..."
 cd ../..
 rm -fr build/
 rm -fr binutils-2.24/
 fi
 
-echo -e "Unpacking gcc-4.8.4.tar.gz..."
+echo -e "$BULLET Unpacking gcc-4.8.4.tar.gz..."
 tar -xf ../tmp/gcc-4.8.4.tar.gz
-echo -e "Unpacking gmp-6.0.0a.tar.lz..."
+echo -e "$BULLET Unpacking gmp-6.0.0a.tar.lz..."
 tar -xf ../tmp/gmp-6.0.0a.tar.lz
-echo -e "Unpacking mpfr-3.1.2.tar.xz..."
+echo -e "$BULLET Unpacking mpfr-3.1.2.tar.xz..."
 tar -xf ../tmp/mpfr-3.1.2.tar.xz
-echo -e "Unpacking mpc-1.0.3.tar.gz..."
+echo -e "$BULLET Unpacking mpc-1.0.3.tar.gz..."
 tar -xf ../tmp/mpc-1.0.3.tar.gz
 
-echo -e "Copying optional packages..."
+echo -e "$BULLET Copying optional packages..."
 mv -f gmp-6.0.0 gcc-4.8.4/gmp
 mv -f mpfr-3.1.2 gcc-4.8.4/mpfr
 mv -f mpc-1.0.3 gcc-4.8.4/mpc
 
-echo -e "Copying configuration files..."
-cp -v gcc/config.sub gcc-4.8.4/config.sub
-cp -v gcc/hydrax.h gcc-4.8.4/gcc/config/hydrax.h
-cp -v gcc/config.gcc gcc-4.8.4/gcc/config.gcc
-cp -v gcc/crossconfig.m4 gcc-4.8.4/libstdc++-v3/crossconfig.m4
-cp -v gcc/config.host gcc-4.8.4/libgcc/config.host
-cp -v gcc/mkfixinc.sh gcc-4.8.4/fixincludes/mkfixinc.sh
+echo -e "$BULLET Applying gcc-4.8.4.patch..."
+cd gcc-4.8.4
+patch -p1 < ../patches/gcc-4.8.4.patch > /dev/null || last_error=1
+if [ "$last_error" == "1" ]; then
+	echo -e "$REDBUL Error applying patches/gcc-4.8.4.patch!!"
+	exit -1
+fi
+cd ..
 
-echo -e "Running autoconf on gcc-4.8.4/libstdc++-v3..."
+#echo -e "Copying configuration files..."
+#cp -v gcc/config.sub gcc-4.8.4/config.sub
+#cp -v gcc/hydrax.h gcc-4.8.4/gcc/config/hydrax.h
+#cp -v gcc/config.gcc gcc-4.8.4/gcc/config.gcc
+#cp -v gcc/crossconfig.m4 gcc-4.8.4/libstdc++-v3/crossconfig.m4
+#cp -v gcc/config.host gcc-4.8.4/libgcc/config.host
+#cp -v gcc/mkfixinc.sh gcc-4.8.4/fixincludes/mkfixinc.sh
+#exit -1
+
+
+echo -e "$BULLET Running autoconf on gcc-4.8.4/libstdc++-v3..."
 cd gcc-4.8.4/libstdc++-v3
 autoconf-2.64
 cd ../..
 
-echo -e "Configuring gcc..."
+echo -e "$BULLET Configuring gcc..."
 mkdir -p build/gcc
 cd build/gcc
 ../../gcc-4.8.4/configure --target=$TARGET --prefix="$PREFIX" \
 	--with-sysroot="$PREFIX" --disable-multilib --disable-nls --enable-languages=c || last_error=1
 if [ "$last_error" == "1" ]; then
-	echo -e "Error in configure gcc!"
+	echo -e "$REDBUL Error in configure gcc!"
 	exit -1
 fi
-echo -e "Building gcc..."
+echo -e "$BULLET Building gcc..."
 make $MAKE_OPTS all-gcc || last_error=1
 if [ "$last_error" == "1" ]; then
-	echo -e "Error in make all-gcc!"
+	echo -e "$REDBUL Error in make all-gcc!"
 	exit -1
 fi
 make $MAKE_OPTS all-target-libgcc || last_error=1
 if [ "$last_error" == "1" ]; then
-	echo -e "Error in make all-target-gcc!"
+	echo -e "$REDBUL Error in make all-target-libgcc!"
 	exit -1
 fi
 make install-gcc || last_error=1
 if [ "$last_error" == "1" ]; then
-	echo -e "Error in make install-gcc!"
+	echo -e "$REDBUL Error in make install-gcc!"
 	exit -1
 fi
 make install-target-libgcc || last_error=1
 if [ "$last_error" == "1" ]; then
-	echo -e "Error in make-install-target-libgcc!"
+	echo -e "$REDBUL Error in make install-target-libgcc!"
 	exit -1
 fi
 

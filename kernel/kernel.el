5280f75c34e2f3e4aa18303bd97138615d010f43
Fernando Rodriguez <frodriguez.developer@outlook.com>
2015-06-25 01:55:46 -0400

    Added ECB Project file

---
+++ kernel/kernel.el
@@ -0,0 +1,9 @@
+;; Object hydrax-kernel
+;; EDE Project Files are auto generated: Do Not Edit
+(ede-cpp-root-project "hydrax-kernel"
+  :file "~/Projects/hydrax/kernel/Makefile"
+  :name "hydrax-kernel"
+  :include-path '("/include"
+		  "/include/sys"
+		 )
+  :targets nil)
